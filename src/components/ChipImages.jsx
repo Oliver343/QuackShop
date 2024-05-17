4b15777ff9463f3f86b39d62baefa3ccfbff3f5f
Oliver343 <93383676+Oliver343@users.noreply.github.com>
2024-05-17 10:55:33 +0000

    Started scale on chip placement

--- src/components/ChipImages.jsx
+++ src/components/ChipImages.jsx
@@ -5,6 +5,9 @@ const ChipImages = (props) => {
   const storeObject = useContext(StoreContextWrapper)
   let smaller = (storeObject.width < storeObject.height) ? storeObject.width : storeObject.height
   let chipSize = (smaller < 1000) ? smaller / 13 : 68
+  let leftSize = (smaller < 1000) ? smaller / 2 : 400
+
+  console.log(leftSize)
 
   return (
     <div>
@@ -12,7 +15,11 @@ const ChipImages = (props) => {
         className={"chips chip" + props.chipSpace}
         src={props.img}
         width={chipSize}
-        style={{maxWidth: "80px"}}
+        style={{
+          maxWidth: "80px",
+          top: "300px",
+          left: leftSize,
+        }}
       ></img>
     </div>
   );
