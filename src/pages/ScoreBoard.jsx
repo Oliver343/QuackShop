ec974293e9fc2c853532857b7ac1ed111e781007
Oliver343 <oliver_343@hotmail.com>
2024-05-23 12:46:12 +0200

    Scoreboard view created

--- src/pages/ScoreBoard.jsx
+++ src/pages/ScoreBoard.jsx
@@ -1,7 +1,22 @@
+import { useContext } from "react";
+import { StoreContextWrapper } from "../store/ContextProvider"
+
+import scoreBoard from "../img/scoreboard.png"
+
 export default function ScoreBoard() {
+    const storeObject = useContext(StoreContextWrapper)
+    let maxValue = ((storeObject.width < storeObject.height ? storeObject.width : storeObject.height) - 4 )
     return(
         <div>
-            SCORE
+            <div 
+                className="newBoard" 
+                style={{backgroundImage: 
+                "url(" + scoreBoard + ")", 
+                backgroundSize: 'contain', 
+                backgroundRepeat: 'no-repeat', 
+                width: 1000, maxWidth: maxValue, 
+                height: 914, maxHeight: maxValue}}
+            ></div>
         </div>
         
     )
